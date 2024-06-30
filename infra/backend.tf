fe908edd8ae10838d8ce25db64731afc6e820f3a
achirazi <chirazi.alexandru@gmail.com>
2024-06-30 19:59:52 +0300

    backend2

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "nexasphere-tf"
-    key     = "prod_terraform.tfstate"
+    key     = "prod_terraform_2.tfstate"
     region  = "eu-central-1"
     encrypt = true
   }
