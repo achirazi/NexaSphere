454bfd63e1b78cdbebf2a7dcb63175eb029dae03
achirazi <chirazi.alexandru@gmail.com>
2024-06-30 19:48:01 +0300

    Uncomment

--- infra/providers.tf
+++ infra/providers.tf
@@ -5,14 +5,14 @@ provider "aws" {
 }
 
 
-# provider "kubernetes" {
-#   host                   = aws_eks_cluster.nexasphere-eks.endpoint
-#   cluster_ca_certificate = base64decode(aws_eks_cluster.nexasphere-eks.certificate_authority[0].data)
+provider "kubernetes" {
+  host                   = aws_eks_cluster.nexasphere-eks.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.nexasphere-eks.certificate_authority[0].data)
 
-#   # exec {
-#   #   api_version = "client.authentication.k8s.io/v1beta1"
-#   #   command     = "aws"
-#   #   # This requires the awscli to be installed locally where Terraform is executed
-#   #   args = ["eks", "get-token", "--cluster-name",  aws_eks_cluster.nexasphere-eks.name]
-#   # }
-# }
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name",  aws_eks_cluster.nexasphere-eks.name]
+  }
+}
